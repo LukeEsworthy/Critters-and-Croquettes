4abd084f06f07ec79458c9679b645c4937848180
Luke Esworthy <lukesworthy@gmail.com>
2020-07-15 15:59:58 -0500

    working on first part of critters and croquettes

---
+++ animals.py
@@ -0,0 +1,171 @@
+from datetime import date
+
+
+class BillyGoat:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+class Otter:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+
+class BoaConstrictor:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+
+class Warthog:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+class Piranha:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+
+class Hellbender:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+
+class Camel:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+class Pike:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+
+class Copperhead:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+
+class Gorilla:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+class Lionfish:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+
+class KingCobra:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+
+class Cheetah:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
+
+
+class MorayEel:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+
+class BlackMamba:
+
+    def __init__(self, name, species):
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+
+bilbo = BillyGoat("Bilbo", "Billy Goat")
+otto = Otter("Otto", "Asian small-clawed otter")
+bo = BoaConstrictor("Bo", "Argentine Boa")
+pumbaa = Warthog("Pumbaa", "Gluttonous warthog")
+snuggles = Piranha("Snuggles", "Red-bellied paranha")
+pinhead = Hellbender("Pinhead", "Ozark hellbender")
+mike = Camel("Mike", "One-humped dromedary")
+lance = Pike("Lance", "Northern pike")
+steve = Copperhead("Steve", "Southern copperhead")
+kong = Gorilla("Kong", "Silverback")
+lonny = Lionfish("Lonny", "Spotfin lionfish")
+jafar = KingCobra("Jafar", "Ophiophagus hannah")
+speedy = Cheetah("Speedy", "South African Cheetah")
+vizzini = MorayEel("Vizzini", "Giant Moray")
+tanya = BlackMamba("Tanya", "Dendroaspis polylepis")
+
+print(bilbo.name)
+print(otto.name)
+print(bo.name)
+print(pumbaa.name)
+print(snuggles.name)
+print(pinhead.name)
+print(mike.name)
+print(lance.name)
+print(steve.name)
+print(kong.name)
+print(lonny.name)
+print(jafar.name)
+print(speedy.name)
+print(vizzini.name)
+print(tanya.name)
+
+print(tanya.date_added)
